bf474cc8c999bb1406acfc1f49c6676e24334664
fumimi23 <highspec.nite@gmail.com>
2025-03-29 15:04:37 +0900

    fix: 誤字修正

--- src/store/relationStore.ts
+++ src/store/relationStore.ts
@@ -1,23 +1,23 @@
 import { type Relation } from '@/schemas/relationSchema';
 import { create } from 'zustand';
 
-interface RelatonStore {
+interface RelationStore {
   relations: Relation[];
   addRelation: (relation: Relation) => void;
   updateRelation: (relation: Relation) => void;
   deleteRelation: (relationId: string) => void;
 }
 
-export const useRelationStore = create<RelatonStore>((set) => ({
+export const useRelationStore = create<RelationStore>((set) => ({
   relations: [],
-  addRelation: (relation: Relation): void => { set((state: RelatonStore) => ({ relations: [...state.relations, relation] })); },
+  addRelation: (relation: Relation): void => { set((state: RelationStore) => ({ relations: [...state.relations, relation] })); },
   updateRelation: (relation: Relation): void => {
-    set((state: RelatonStore) => ({
+    set((state: RelationStore) => ({
       relations: state.relations.map((r) => { return r.id === relation.id ? relation : r; }),
     }));
   },
   deleteRelation: (relationId: string): void => {
-    set((state: RelatonStore) => ({
+    set((state: RelationStore) => ({
       relations: state.relations.filter((r) => r.id !== relationId),
     }));
   },
