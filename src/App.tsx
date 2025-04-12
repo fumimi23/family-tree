e988877e14bc76b5159acdcaa520e889e2a8e3c0
fumimi23 <highspec.nite@gmail.com>
2025-04-13 01:58:03 +0900

    [#19]feat: データインポート機能追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import '@/App.css';
 import { PeopleTable } from '@/components/person/PeopleTable';
 import { RelationTable } from '@/components/relation/RelationTable';
 import { H1 } from '@/components/ui/H1';
+import { PrimaryButton } from '@/components/ui/PrimaryButton';
 import { usePeopleStore } from '@/store/personStore';
 import { useRelationStore } from '@/store/relationStore';
-import { Flex, Grid, GridItem, Link } from '@chakra-ui/react';
+import { Flex, Grid, GridItem, Input, Link } from '@chakra-ui/react';
 import React from 'react';
 
 function App(): React.ReactNode {
@@ -17,6 +18,35 @@ function App(): React.ReactNode {
     return URL.createObjectURL(blob);
   },
   [people, relations]);
+
+  const handleFileChange = React.useCallback((e: React.ChangeEvent<HTMLInputElement>): void => {
+    if (!e.target.files) {
+      return;
+    }
+
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (event): void => {
+      try {
+        const jsonString = event.target?.result as string;
+        const jsonData = JSON.parse(jsonString);
+        const { people: newPeople, relations: newRelations } = jsonData;
+        usePeopleStore.setState({ people: newPeople });
+        useRelationStore.setState({ relations: newRelations });
+      } catch (error) {
+        console.error('JSONの読み込みに失敗しました。', error);
+      }
+    };
+    reader.readAsText(file);
+  }, []);
+
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const handleImportClick = React.useCallback((): void => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  },
+  []);
 
   return (
     <>
@@ -37,6 +67,10 @@ function App(): React.ReactNode {
               >
                 エクスポート
               </Link>
+
+              <PrimaryButton onClick={handleImportClick}>
+                インポート
+              </PrimaryButton>
             </Flex>
           </Flex>
         </GridItem>
@@ -50,6 +84,14 @@ function App(): React.ReactNode {
         </GridItem>
 
       </Grid>
+
+      <Input
+        accept=".json"
+        display="none"
+        onChange={handleFileChange}
+        ref={inputRef}
+        type="file"
+      />
     </>
   );
 }
